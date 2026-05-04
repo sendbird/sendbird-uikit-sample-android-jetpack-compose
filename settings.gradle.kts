baf421270be4dbf658f6e1bbff76fa850d142fac
sha.sdk_deployment <sha.sdk_deployment@sendbird.com>
2026-05-04 15:01:25 +0900

    Revert "Merge pull request #16 from sendbird/release/compose/1.3.5"
    This reverts commit f1afda70f2cf874bc20f95a12869f94ec197cac0, reversing
    changes made to ad19a57f52150825e16a2368898b9da65cef4a18.

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://repo.sendbird.com/public/maven") }
+    }
+}
+
+rootProject.name = "uikit-compose-sample"
+include(":uikit-compose-sample")
